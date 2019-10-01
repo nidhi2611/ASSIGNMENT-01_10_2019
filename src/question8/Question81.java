a92c6a617835e0ed70ed4884aa76f20981c55d89
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2019-10-01 22:48:56 +0530

    string inputted

--- src/question8/Question81.java
+++ src/question8/Question81.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Question81 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String str = sc.next();
 
     }
 }

3dbfb41e4e1998986220d1420e5dec207b81d330
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2019-10-02 00:31:39 +0530

    input taken and constructor created

--- src/question11/Question111.java
+++ src/question11/Question111.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class Question111 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String string = sc.next();
+        char ch = sc.next().trim().charAt(0);
+        Question112 obj = new Question112(string, ch);
+
 
     }
 }

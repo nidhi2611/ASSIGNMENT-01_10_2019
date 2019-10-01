09eb1044bb485685ac9d05c46bf7ade9af7eac00
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2019-10-01 22:56:28 +0530

    function created

--- src/question8/Question82.java
+++ src/question8/Question82.java
@@ -11,4 +11,18 @@ public class Question82 {
         this.string = string;
     }
 
+    Question82(String string) {
+        this.string = string;
+    }
+
+    public int upper_case_weight() {
+        int k = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (Character.isUpperCase(string.charAt(i))) {
+                k = k + (int) string.charAt(i);
+            }
+
+        }
+    }
+
 }

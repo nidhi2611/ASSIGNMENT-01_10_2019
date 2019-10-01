8387f401800cd7b136a18eaf7bd7bad7bdb8e9da
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2019-10-02 00:15:43 +0530

    scanner object created

--- src/question10/Question101.java
+++ src/question10/Question101.java
@@ -1,7 +1,10 @@
 package question10;
 
+import java.util.Scanner;
+
 public class Question101 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
     }
 }

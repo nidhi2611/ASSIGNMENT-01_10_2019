f57349a585ba133fa31d5a1821c2ba3863f75de3
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2019-10-01 22:26:35 +0530

    getter setter created

---
+++ src/question7/Question72.java
@@ -0,0 +1,14 @@
+package question7;
+
+public class Question72 {
+    protected String string;
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+}

75dd1e69f9d6173d56ec0f748b1ea74dbbae54a7
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2019-10-01 19:50:55 +0530

    constructor created in defination class

---
+++ src/question1/Question12.java
@@ -0,0 +1,11 @@
+package question1;
+
+public class Question12 {
+    protected String input_string;
+
+    Question12(String input_string) {
+        this.input_string = input_string;
+    }
+
+}
+}

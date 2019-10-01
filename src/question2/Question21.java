73f0c7ff2c15e7a57c780e9f8522bf6b2a09e364
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2019-10-01 20:17:38 +0530

    values inputted and passed to defination class

--- src/question2/Question21.java
+++ src/question2/Question21.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class Question21 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String string = sc.next();
+        int index = sc.nextInt();
+        Question22 obj = new Question22(string, index);
 
     }
 }

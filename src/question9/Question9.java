4f0260125b9bee7d5505e6bbbce008ef4371eed8
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2019-10-01 23:27:28 +0530

    constructor created

--- src/question9/Question9.java
+++ src/question9/Question9.java
@@ -6,6 +6,6 @@ public class Question9 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String string = sc.next();
-
+        Question91 obj = new Question91(string);
     }
 }
---
+++ src/question9/Question91.java
@@ -0,0 +1,17 @@
+package question9;
+
+public class Question91 {
+    protected String string;
+
+    Question91(String str) {
+        this.string = str;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+}

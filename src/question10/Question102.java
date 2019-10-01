392d53c98d89ec733c5983aa3e7ef0f89a7bf4dc
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2019-10-02 00:23:17 +0530

    funtion created

--- src/question10/Question102.java
+++ src/question10/Question102.java
@@ -2,17 +2,29 @@ package question10;
 
 public class Question102 {
     protected String string;
-
+    protected char ch;
     public String getString() {
         return string;
+    }
+
+    Question102(String string, char ch) {
+        this.string = string;
+        this.ch = ch;
     }
 
     public void setString(String string) {
         this.string = string;
     }
 
-    Question102(String string) {
-        this.string = string;
+    public char getCh() {
+        return ch;
     }
 
+    public void setCh() {
+        this.ch = ch;
+    }
+
+    public int index() {
+        return string.indexOf(ch);
+    }
 }

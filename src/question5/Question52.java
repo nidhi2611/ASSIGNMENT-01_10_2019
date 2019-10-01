16abc369689b8babf7459b2a87c69cd784fe23cb
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2019-10-01 22:17:14 +0530

    output method created

--- src/question5/Question52.java
+++ src/question5/Question52.java
@@ -1,4 +1,21 @@
 package question5;
 
 public class Question52 {
+    protected String string;
+
+    Question52(String string) {
+        this.string = string;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    public String output() {
+        return string.toUpperCase();
+    }
 }

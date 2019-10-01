14edc71a23c679c8d0e6bcba1c2ac5a2b96ce307
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2019-10-01 20:15:23 +0530

    new question Scanner class created

--- src/question1/Question11.java
+++ src/question1/Question11.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Question11 {
     public static void main(String[] agrs) {
         Scanner sc = new Scanner(System.in);
-
+        String string = sc.next();
+        Question12 obj = new Question12(string);
+        obj.modify();
     }
 }
--- src/question1/Question12.java
+++ src/question1/Question12.java
@@ -3,9 +3,30 @@ package question1;
 public class Question12 {
     protected String input_string;
 
-    Question12(String input_string) {
+    public Question12(String string) {
+        input_string = string;
+
+    }
+
+    public String getInput_string() {
+        return input_string;
+    }
+
+    public void setInput_string(String input_string) {
         this.input_string = input_string;
     }
 
-}
+    public void modify() {
+        System.out.print("index:    ");
+        for (int i = 0; i < input_string.length(); i++) {
+            System.out.print("\t" + i);
+        }
+        System.out.println();
+        System.out.print("chars:    ");
+        for (int j = 0; j < input_string.length(); j++) {
+            System.out.print("\t" + input_string.charAt(j));
+        }
+        System.out.println();
+
+    }
 }

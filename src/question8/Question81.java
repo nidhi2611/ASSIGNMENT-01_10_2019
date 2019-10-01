80a7de4f6d9f9259c6bbc318c22baf6b023058f8
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2019-10-01 22:50:48 +0530

    getter setter created

--- src/question8/Question81.java
+++ src/question8/Question81.java
@@ -6,6 +6,6 @@ public class Question81 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
-
+        Question82 obj = new Question82(str);
     }
 }
---
+++ src/question8/Question82.java
@@ -0,0 +1,14 @@
+package question8;
+
+public class Question82 {
+    protected String string;
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+}

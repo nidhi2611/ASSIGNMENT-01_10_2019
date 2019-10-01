2ae5b45e9ce62a15cbc91c29b85614962212def0
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2019-10-02 00:29:15 +0530

    Scanner class object created

--- src/question11/Question111.java
+++ src/question11/Question111.java
@@ -1,7 +1,10 @@
 package question11;
 
+import java.util.Scanner;
+
 public class Question111 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
     }
 }

f735ec826dbc26a802915e2a5cf4495616875d78
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2019-10-02 00:34:26 +0530

    getter setter defined

--- src/question11/Question112.java
+++ src/question11/Question112.java
@@ -1,4 +1,31 @@
 package question11;
 
 public class Question112 {
+    protected String string;
+    protected char ch;
+
+    Question112(String string, char ch) {
+        this.string = string;
+        this.ch = ch;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    public char getCh() {
+        return ch;
+    }
+
+    public void setCh(char ch) {
+        this.ch = ch;
+    }
+
+    public void output() {
+
+    }
 }

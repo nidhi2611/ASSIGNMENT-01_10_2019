ffc892f395f8d548785522b2ad9b8fe973e142e4
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2019-10-02 00:24:40 +0530

    final commit

--- src/question10/Question101.java
+++ src/question10/Question101.java
@@ -6,7 +6,9 @@ public class Question101 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String string = sc.next();
-        Question102 obj = new Question102(string);
+        char ch = sc.next().trim().charAt(0);
+        Question102 obj = new Question102(string, ch);
+        System.out.print(obj.index());
 
     }
 }
